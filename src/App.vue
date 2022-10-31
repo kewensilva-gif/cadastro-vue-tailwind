9b577172a11355b883d9d896ce099634cae9dbcf
Kewen Silva <silvakewen52@gmail.com>
2022-10-31 17:17:20 -0300

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="w-screen h-screen flex items-center justify-center dark:bg-gray-900 bg-gray-50">
+  <div class="bg-white shadow-xl w-full sm:10/12 md:w-8/12 lg:w-6/12 p-6">
+    <h4 class="font-semibold text-md mb-3 border-b border-gray-300">Login</h4>
+
+    <div class="space-y-3">
+        <input 
+          type="text"
+          class="px-3 py-2 placeholder-gray-200 border border-gray-300 w-full"
+          placeholder="E-mail"
+          >
+
+        <input 
+          type="password"
+          class="px-3 py-2 placeholder-gray-200 border border-gray-300 w-full"
+          placeholder="Senha"
+        >
+
+        <div class="text-right">
+          <button class="rounded px-3 py-2 bg-green-400 hover:bg-green-500 text-green-900">Login</button>
+        </div>
+      </div>
+  </div>
+    <DarkMode />
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DarkMode from "./components/DarkMode.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    DarkMode
   }
+
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/DarkMode.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <button @click="toggle" class="cursor-pointer absolute top-10 dark:bg-white dark:text-black text-white bg-black rounded p-4">{{ name }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DarkMode",
+    data(){
+        return {
+            isDark: false,
+        }
+    },  
+    computed: {
+        name() {
+            return this.isDark ? 'Light' : 'Dark'
+        }
+    },
+    watch: {
+        isDark(vl){
+            const html = document.querySelector("html");
+            if(vl){
+                html.classList.add("dark")
+            } else {
+                html.classList.remove("dark")
+            }
+        }
+    },
+
+    methods: {
+        toggle(){
+            this.isDark = !this.isDark
+        }
+    }
+}
+</script>
